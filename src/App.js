62e0d5e2e34c49c4733423e9cafa7b3517aed501
Florian Aumeier (FG-230) <Florian.AB.Aumeier@bmw.de>
2020-01-24 22:46:36 +0100

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {ThemeProvider} from './contexts/theme'
+import Loading from './components/Loading'
+import Nav from './components/Nav'
 import './App.css';
+import Post from './components/Post'
+import User from './components/User'
+import Posts from './components/Posts'
 
 function App() {
+  const [theme, setTheme] = useState('light')
+  const toggleTheme = () => {
+    setTheme(
+      theme === 'light' ? 'dark' : 'light'
+    )
+  }
+
+  const state = {theme, toggleTheme}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <ThemeProvider value={state}>
+        <div className={theme}>
+          <div className='container'>
+            <Nav/>
+
+            <React.Suspense fallback={<Loading/>}>
+              <Switch>
+                <Route
+                  exact
+                  path='/'
+                  render={() => <Posts type='top'/>}
+                />
+                <Route
+                  path='/new'
+                  render={() => <Posts type='new'/>}
+                />
+                <Route path='/post' component={Post}/>
+                <Route path='/user' component={User}/>
+                <Route render={() => <h1>404</h1>}/>
+              </Switch>
+            </React.Suspense>
+          </div>
+        </div>
+      </ThemeProvider>
+    </Router>
+  )
 }
 
 export default App;
